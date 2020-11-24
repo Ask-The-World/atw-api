d26c6fb039223225dd03bae8e26aca16b64dbab4
TecTrixer <tonihoevedes@gmail.com>
2020-11-24 19:58:10 +0100

    first stable but unusable implementation of actix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod conf_vars;
 use mongodb::{Client};
-use tokio;
+use actix_web::{get, web, App, HttpServer, Responder};
 mod db;
 
-#[tokio::main]
+#[actix_web::main]
 pub async fn main() -> mongodb::error::Result<()>{
 
     // Get configuration
@@ -19,5 +19,15 @@ pub async fn main() -> mongodb::error::Result<()>{
     for db_name in client.list_database_names(None, None).await? {
         println!("{}", db_name);
     }
+
+    #[get("/{id}")]
+    pub async fn index(web::Path(id): web::Path<u32>)-> impl Responder{
+        format!("Hello {}!, How are you?", id)
+    }
+    HttpServer::new(|| App::new().service(index))
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await?;
+
     return Ok(());
 }
