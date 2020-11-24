034b6bf5dccb7f120c6fdbab78247187f64c827c
TecTrixer <tonihoevedes@gmail.com>
2020-11-24 19:42:05 +0100

    extracted database interaction to db.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod conf_vars;
-use mongodb::{Client, bson::doc};
+use mongodb::{Client};
 use tokio;
+mod db;
 
 #[tokio::main]
 pub async fn main() -> mongodb::error::Result<()>{
@@ -9,14 +10,10 @@ pub async fn main() -> mongodb::error::Result<()>{
     let config: conf_vars::ConfVars = conf_vars::get_conf_vars();
     println!("{}, {}, {}, {}, {},", config.min_time, config.max_time, config.default_time, config.max_question_length, config.default_delete_time);
 
-    let client = Client::with_uri_str(&format!("mongodb://{}:{}@{}:{}/", config.db_user, config.db_password, config.db_server, config.db_port)[..]).await?;
+    let client: Client = db::get_client().await?;
 
-    // Ping the server to see if you can connect to the cluster
-    client
-        .database("admin")
-        .run_command(doc! {"ping": 1}, None)
-        .await?;
-    println!("Connected successfully.");
+    db::ping_server(&client).await?;
+    
 
     // List the names of the databases in that deployment.
     for db_name in client.list_database_names(None, None).await? {
