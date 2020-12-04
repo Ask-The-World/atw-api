42a71c896f70a70a2a897ea19243d122189fd1e5
TecTrixer <tonihoevedes@gmail.com>
2020-12-04 20:51:24 +0100

    added QuestionEntry structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod conf_vars;
 use actix_web::{web, App, HttpServer, Responder};
-use mongodb::{Collection, bson::Document};
+use mongodb::{Collection, bson::{Document, oid}};
 mod db;
 use futures::stream::StreamExt;
+use serde::{Serialize, Deserialize};
+use std::time::SystemTime;
 
 #[actix_web::main]
 pub async fn main() -> mongodb::error::Result<()> {
@@ -10,9 +12,27 @@ pub async fn main() -> mongodb::error::Result<()> {
         collection: Collection,
     }
 
+    let mut now: u64 = 0;
+
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(time) => now = time.as_secs(),
+        Err(_) => {}
+    }
+    #[derive(Deserialize, Serialize)]
+    struct QuestionEntry {
+        #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+        id: Option<oid::ObjectId>,
+        question: String,
+        time: u32,
+        yes: u32,
+        no: u32,
+        default_answer: bool,
+        timestamp: u64,
+    }
+
     let collection = db::get_collection().await.unwrap();
 
-    println!("Successfully running... \nStop with \"CTRL + C\"...");
+    println!("Successfully running... \nStarted at {}...\nStop with \"CTRL + C\"...", now);
 
     async fn submit_question(
         web::Path(param): web::Path<(String, u32)>,
